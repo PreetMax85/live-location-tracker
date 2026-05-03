dac19dbc99fb42d7ea0d06f938b0196ab6f69eb4
PreetMax85 <preetjain2200@gmail.com>
2026-05-03 16:01:35 +0530

    cleaup

--- kafka-client.js
+++ kafka-client.js
@@ -2,24 +2,7 @@ import { Kafka } from 'kafkajs';
 
 process.env.KAFKAJS_NO_PARTITIONER_WARNING = '1';
 
-const brokers = process.env.KAFKA_BROKERS
-  ? process.env.KAFKA_BROKERS.split(',')
-  : ['localhost:9092'];
-
-const sasl =
-  process.env.KAFKA_USERNAME && process.env.KAFKA_PASSWORD
-    ? {
-        mechanism: 'scram-sha-256',
-        username: process.env.KAFKA_USERNAME,
-        password: process.env.KAFKA_PASSWORD,
-      }
-    : null;
-
-const ssl = !!sasl;
-
 export const kafkaClient = new Kafka({
   clientId: 'chaicode',
-  brokers,
-  ssl,
-  sasl,
+  brokers: process.env.KAFKA_BROKERS ? process.env.KAFKA_BROKERS.split(',') : ['localhost:9092'],
 });
